49352a322535c67d1a4d2459244b8ecb2d2b2c53
Howie2526 <59353609+Howie2526@users.noreply.github.com>
2020-01-04 22:07:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 session_start();
 $server = $_SERVER['SERVER_NAME'];
 $request = $_SERVER['REQUEST_URI'];
-$canonical = '/stripe/';
+$canonical = '/credit-card-donation/';
 if ($server === "localhost") {
 	$root = $_SERVER['DOCUMENT_ROOT'] . "/cdunion/";
 	$base = 'http://localhost/cdunion/';
 } elseif (($request !== $canonical) || ($_SERVER['HTTPS'] != 'on')) {
-	header ('Location: https://cdunion.ca/stripe/', true, 301);
+	header ('Location: https://cdunion.ca/credit-card-donation/', true, 301);
 } else {
-	$root = $_SERVER['DOCUMENT_ROOT'];
+	$root = $_SERVER['DOCUMENT_ROOT'] . "/";
 	$base = 'https://cdunion.ca/';
 }
-include ($root . '/stripe/body.php');
+include ($root . 'credit-card-donation/body.php');
 die();
 ?>
