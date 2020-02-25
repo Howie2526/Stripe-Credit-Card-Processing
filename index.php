8eb8bdf2205f4a5d093031e9edfd02ea32f4fc49
Harold Indoe <59353609+Howie2526@users.noreply.github.com>
2020-02-25 10:57:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <?php
+
+header('Strict-Transport-Security:max-age=63072000;includeSubDomains;preload');
+header('X-Frame-Options:SAMEORIGIN');
+header('Referrer-Policy:same-origin');
+header('X-XSS-Protection:1;mode=block;');
+ini_set('session.cookie_httponly','1');
+ini_set('session.cookie_secure','1');
+ini_set('session.cookie_samesite','Strict');
 session_start();
-$server = $_SERVER['SERVER_NAME'];
-$request = $_SERVER['REQUEST_URI'];
-$canonical = '/credit-card-donation/';
-if ($server === "localhost") {
-	$root = $_SERVER['DOCUMENT_ROOT'] . "/cdunion/";
-	$base = 'http://localhost/cdunion/';
-} elseif (($request !== $canonical) || ($_SERVER['HTTPS'] != 'on')) {
-	header ('Location: https://cdunion.ca/credit-card-donation/', true, 301);
-} else {
-	$root = $_SERVER['DOCUMENT_ROOT'] . "/";
-	$base = 'https://cdunion.ca/';
+
+$ReqSer=$_SERVER['SERVER_NAME'];
+$ReqURL=$_SERVER['REQUEST_URI'];
+$ReqURL=$ReqSer.$ReqURL;
+$LocURL='localhost/cdunion/credit-card-donation/';
+$CanURL='cdunion.ca/credit-card-donation/';
+if($ReqURL===$LocURL){
+	$BaseURL='http://localhost/cdunion/';
+	$Root="{$_SERVER['DOCUMENT_ROOT']}/cdunion/";
+}elseif(($ReqURL===$CanURL)&&($_SERVER['HTTPS']='on')){
+	$BaseURL='https://cdunion.ca/';
+	$Root="{$_SERVER['DOCUMENT_ROOT']}/";
+}else{
+	header("Location:https://$CanURL");
+	exit();
 }
-include ($root . 'credit-card-donation/body.php');
+ob_start();
+include('body.php');
+$body=ob_get_contents();
+ob_end_clean();
+$remove=array('/\t/','/\v/');
+$replace='';
+$body=preg_replace($remove,$replace,$body);
+echo($body);
 exit();
 ?>
